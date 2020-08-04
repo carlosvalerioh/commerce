031c19fa7220fd24c14ef1837a415af51bd4b907
Carlos Valerio <cvalerio778@gmail.com>
2020-08-04 18:05:25 -0500

    First commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Root from '../components/common/Root';
 import Footer from '../components/common/Footer';
 import SocialMedia from '../components/common/SocialMedia';
-import ExploreBanner from '../components/productAssets/ExploreBanner';
+
 import HeroSection from '../components/homepage/HeroSection';
 import HomeBanner from '../components/homepage/HomeBanner';
 import CategoryBanner from '../components/homepage/CategoryBanner';
@@ -17,9 +17,9 @@ const Home = () => (
 
     <HeroSection />
     <HomeBanner />
-    <CategoryBanner />
+    {/* <CategoryBanner /> */}
     <ProductsBanner />
-    <ExploreBanner />
+    {/* <ExploreBanner /> */}
     <SocialMedia />
     <Footer />
   </Root>
